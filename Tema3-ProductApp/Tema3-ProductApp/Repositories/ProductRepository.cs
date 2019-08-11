238e0613f268c43b92363ad4bac5997192889165
aldeamiruna <aldea.miruna32@gmail.com>
2019-08-11 23:06:56 +0300

    Added the last two homework

---
+++ Tema3-ProductApp/Tema3-ProductApp/Repositories/ProductRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tema3_ProductApp.Repositories;
+
+namespace Tema3_ProductApp.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        readonly IEnumerable<Product> _products = new List<Product>
+        {
+            new Product
+            {
+                Id = 1,
+                Name = "IceCream",
+                Description = "is so yummi"
+
+            },
+            new Product
+
+            {
+                Id = 2,
+                Name = "French fries",
+                Description = " are the best"
+
+            },
+            new Product
+            {
+                Id = 3,
+                Name = "Salad",
+                Description = " is healthy"
+
+            }
+
+        };
+
+        public IEnumerable<Product> Products => _products;
+
+
+        public Product GetById(int id)
+        {
+            return _products.SingleOrDefault(x => x.Id == id);
+        }
+    }
+}
+
---
+++ Tema4_ex2/Tema4_ex2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace Tema4_ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string textFile = "C:\\Users\\MA073495\\Desktop\\Cursuri\\C#\\Tema4\\Tema4_ex2\\vector.txt";
+
+            using (StreamReader file = new StreamReader(textFile))
+            {
+                int counter = 0;
+                string line;
+                List<int> list = new List<int>();
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    list.Add(int.Parse(line));
+                    counter++;
+                }
+
+                int[] arr = list.ToArray();
+                Array.Sort(arr);
+                file.Close();
+                foreach(int item in arr)
+                {
+                    Console.WriteLine("item =" + item.ToString());
+                }
+                Console.WriteLine($"File has {counter} lines.");
+            }
+        }
+    }
+}
